9355e8fedd7275532af4d14c4a3379d88869b651
Derek Riley <derektriley@comcast.net>
2015-02-09 15:58:02 -0700

    Finished Point3D

--- src/math/Point3D.java
+++ src/math/Point3D.java
@@ -11,4 +11,60 @@ package math;
  */
 public class Point3D {
     
+    public double x, y, z;
+    
+    public Point3D() {}
+    
+    public Point3D(final double a) {
+        this.x = a;
+        this.y = a;
+        this.z = a;
+    }
+    
+    public Point3D(final double a, final double b, final double c) {
+        this.x = a;
+        this.y = b;
+        this.z = c;
+    }
+    
+    public Vector3D sub(Point3D p) {
+        Vector3D temp = new Vector3D();
+        temp.x = x - p.x;
+        temp.y = y - p.y;
+        temp.z = z - p.z;
+        return temp;
+    }
+    
+    public Point3D add(Vector3D v) {
+        Point3D temp = new Point3D();
+        temp.x = x + v.x;
+        temp.y = y + v.y;
+        temp.z = z + v.z;
+        return temp;
+    }
+    
+    public Point3D sub(Vector3D v) {
+        Point3D temp = new Point3D();
+        temp.x = x - v.x;
+        temp.y = y - v.y;
+        temp.z = z - v.z;
+        return temp;
+    }
+    
+    public Point3D mult(final double a) {
+        Point3D temp = new Point3D();
+        temp.x = x * a;
+        temp.y = y * a;
+        temp.z = z * a;
+        return temp;
+    }
+    
+    public double d_squared(final Point3D p) {
+        return Math.pow(distance(p), 2.0d);
+    }
+    
+    public double distance(final Point3D p) {
+        return Math.sqrt(((p.x - x) * (p.x - x)) + ((p.y - y) * (p.y - y)) + ((p.z - z) * (p.z - z)));
+    }
+    
 }
--- src/world/World.java
+++ src/world/World.java
@@ -7,7 +7,9 @@ package world;
 
 import java.awt.Color;
 import java.util.List;
+import math.Point3D;
 import math.Ray;
+import math.Vector3D;
 import objects.GeometricObject;
 import objects.Sphere;
 import tracers.Tracer;
@@ -38,7 +40,23 @@ public class World {
     }
     
     public void render_scene() {
-    
+        Color pixel_color;
+        Ray ray = new Ray();
+        int hres = vp.hres;
+        int vres = vp.vres;
+        float s = vp.s;
+        float zw = 100.0f; //hardwired in
+        
+        ray.d = new Vector3D(0.0f, 0.0f, -1.0f);
+        
+        for (int r  = 0; r < vres; r++) {
+            for (int c = 0; c <= hres; c++) {
+                ray.o = new Point3D(s * (c - hres / 2.0f + 0.5f), s * (r- vres / 2.0f + 0.5), zw);
+                
+            }
+        }
+        
+        
     }
  
     public void render_scene(int start_x, int start_y) {
